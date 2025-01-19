3d8bf172559514f92abf1e06c4631c5f445dfa17
sh1ggy <tyronewessnolasco@gmail.com>
2025-01-19 15:47:22 +1000

    heardle

--- app.py
+++ app.py
@@ -7,34 +7,83 @@ import random
 app = Flask(__name__)
 
 IMAGE_DIR = "./static/images"
-songs = []
+AUDIO_DIR = "./static/audio"
+
+songImages = []
+songAudio = []
+
 
 @app.route("/")
 def home():
-    with open('./templates/index.html', 'r') as file:
-        return file.read();
+    with open("./templates/index.html", "r") as file:
+        return file.read()
 
-@app.route('/static/images/<filename>')
+
+@app.route("/jacket")
+def jacket():
+    with open("./templates/game/jacket.html", "r") as file:
+        return file.read()
+
+
+@app.route("/track")
+def track():
+    with open("./templates/game/track.html", "r") as file:
+        return file.read()
+
+
+@app.route("/static/images/<filename>")
 def send_image(filename):
     return send_from_directory(IMAGE_DIR, filename)
 
-@app.route("/api/songs")
+
+@app.route("/static/audio/<filename>")
+def send_audio(filename):
+    return send_from_directory(AUDIO_DIR, filename)
+
+
+@app.route("/api/images/songs")
 def get_songs():
     query = request.args.get("query", "").lower()
-    suggestions = [song['title'] for song in songs if query in song['title'].lower()];
+    suggestions = [
+        song["title"] for song in songImages if query in song["title"].lower()
+    ]
     return json.dumps(suggestions)
 
-@app.route("/api/random-song")
-def get_random_song():
-    return json.dumps(random.choice(songs))
 
-def make_song_object_from_file(file):
+@app.route("/api/audio/songs")
+def get_audio():
+    query = request.args.get("query", "").lower()
+    suggestions = [
+        song["title"] for song in songAudio if query in song["title"].lower()
+    ]
+    return json.dumps(suggestions)
+
+
+@app.route("/api/images/random-song")
+def get_random_image_song():
+    return json.dumps(random.choice(songImages))
+
+
+@app.route("/api/audio/random-song")
+def get_random_audio_song():
+    return json.dumps(random.choice(songAudio))
+
+
+def make_song_images_object_from_file(file):
+    return {"title": file.split("-jacket")[0], "imagesUrl": f"/static/images/{file}"}
+
+
+def make_song_audio_object_from_file(file):
+    title = file.split("-song")[0]
     return {
-        "title": file.split("-jacket")[0],
-        "coverUrl": f"/static/images/{file}",
+        "title": title,
+        "audioUrl": f"/static/audio/{file}",
+        "imagesUrl": f"/static/images/{title}-jacket.png",
     }
 
 if __name__ == "__main__":
-    songs = os.listdir(IMAGE_DIR)
-    songs = [make_song_object_from_file(file) for file in songs]
-    app.run(debug=False, host='0.0.0.0') 
+    songImages = os.listdir(IMAGE_DIR)
+    songAudio = os.listdir(AUDIO_DIR)
+    songImages = [make_song_images_object_from_file(file) for file in songImages]
+    songAudio = [make_song_audio_object_from_file(file) for file in songAudio]
+    app.run(debug=False, host="0.0.0.0", port=5001)
